ba62273e5357e0c5f491861e1c8aed7e2a399810
Josh Gribbon <josh.p.grib@gmail.com>
2019-01-26 11:16:07 -0500

    Add most of hw1

--- cs523w1-iot-ios/hw1-intro-to-swift/hw1.swift
+++ cs523w1-iot-ios/hw1-intro-to-swift/hw1.swift
@@ -1,22 +1,62 @@
-func getLoanStatus (remainingAmount: Double, monthCount: Int) -> Bool {
-    print(remainingAmount)
-    if (remainingAmount < 100) {
-        print("Done")
-        return true
-    }
-    let payment: Double = 100
-    let newRemaining = remainingAmount - payment
-    let newMonth = monthCount + 1
-    print("Month ",  newMonth,  ": ",  newRemaining)
-    return getLoanStatus(remainingAmount: newRemaining, monthCount: newMonth)
-}
-
-let LOAN_AMOUNT: Double = 1000
-let MONTH_START : Int = 0
 func ex1() -> Bool {
-    getLoanStatus(remainingAmount: LOAN_AMOUNT, monthCount: MONTH_START)
-    print(1000)
-    return true
+  let loanAmount: Double = 1000.0
+  var remaining: Double = loanAmount
+  var month: Int = 0
+
+  while(remaining > 0) {
+    // add interest
+    var interestRate: Double = (remaining > 500.0) ? 1.02 : 1.01
+    remaining *= interestRate
+    // make the payment
+    var payment: Double = (remaining < 100) ? remaining : 100.0
+    remaining -= payment
+    // output
+    print("Month \(month): Paid \(payment), \(remaining) remaining")
+    // prep for next loop
+    month = month + 1
+  }
+  return true
 }
 
+func getRunningStatus() -> String {
+  return "Hey"
+}
+
+func ex2() -> Bool {
+  let Weather = 20
+  let Schedule = 0
+  let LikeRunning = 0
+  print(getRunningStatus())
+  return true
+}
+
+func ex3() -> Bool {
+  let runnerSpeeds: [String: Int] = [
+    "Elena": 341, "Thomas": 273, "Hamilton": 278, "Suzie": 329, 
+    "Phil": 445, "Matt": 402, "Alex": 388, "Emma": 275, 
+    "John": 243, "James": 334, "Jane": 412, "Emily": 393,
+    "Daniel": 299, "Neda": 343, "Aaron": 317, "Kate": 265
+  ]
+  var fastestSpeed = Int.max
+  var fastestRunner = ""
+  for (runner, speed) in runnerSpeeds {
+    if (speed < fastestSpeed) {
+      fastestSpeed = speed
+      fastestRunner = runner
+    }
+  }
+  print("Fastest runner is \(fastestRunner) with a time of \(fastestSpeed).")
+  return true
+}
+
+print()
+print("Exercise 1:")
 ex1()
+
+print()
+print("Execise 2:")
+ex2()
+
+print()
+print("Execise 3:")
+ex3()
