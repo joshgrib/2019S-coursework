4b2a269465b4e4d4e9f432001b0a0f94064a7876
user150937 <user150937@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-09 17:43:09 -0500

    Add web browser code up to challenges

---
+++ cs523w1-iot-ios/WebBrowser/WebBrowser/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by user150937 on 2/9/19.
+//  Copyright © 2019 Josh Gribbon. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var urlTextField: UITextField!
+    
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var forwardButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.navigationDelegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let urlString:String = "http://www.apple.com"
+        let url:URL = URL(string: urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        urlTextField.text = urlString
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let urlString:String = urlTextField.text!
+        let url:URL = URL(string: urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        urlTextField.text = urlString
+        return true
+    }
+    @IBAction func backButtonTapped(_ sender: Any) {
+        if webView.canGoBack{
+            webView.goBack()
+        }
+    }
+    @IBAction func forwardButtonTapped(_ sender: Any) {
+        if webView.canGoForward{
+            webView.goForward()
+        }
+    }
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        urlTextField.text = webView.url?.absoluteString
+    }
+}
+
