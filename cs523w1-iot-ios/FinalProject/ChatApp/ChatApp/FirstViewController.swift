64a3b6adf0c2d4b8458dfb9d4a27a8802a407f99
user150937 <user150937@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-24 21:28:27 -0500

    Add initial project

---
+++ cs523w1-iot-ios/FinalProject/ChatApp/ChatApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChatApp
+//
+//  Created by user150937 on 2/24/19.
+//  Copyright © 2019 Josh Gribbon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cs523w1-iot-ios/FinalProject/ChatApp/ChatApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChatApp
+//
+//  Created by user150937 on 2/24/19.
+//  Copyright © 2019 Josh Gribbon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
